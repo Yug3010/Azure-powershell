310529fa9a06bca407549efb1b41caab33417834
Yug Sutariya <yugsutariya@yugs-air.home>
2025-05-21 15:45:45 -0400

    changes

--- create-vm.ps1
+++ create-vm.ps1
@@ -1,12 +1,39 @@
-# Variables
-$resourceGroup = "MyResourceGroup"
-$location = "EastUS"
-$vmName = "AutoVM01"
+# create-vm.ps1
 
-# Create Resource Group
+Write-Host "Starting VM creation process..."
+
+# Check if connected to Azure
+if (-not (Get-AzContext)) {
+    Write-Error "Not connected to Azure. Aborting script."
+    exit 1
+}
+
+# Parameters
+$resourceGroup = "PSAutomationRG"
+$location = "EastUS"
+$vmName = "PSAutoVM"
+$vmSize = "Standard_DS1_v2"
+$adminUsername = "yugsutariya"
+$adminPassword = ConvertTo-SecureString "Yugsutariya@3010" -AsPlainText -Force
+
+# Create resource group
+Write-Host "Creating resource group..."
 New-AzResourceGroup -Name $resourceGroup -Location $location
 
-# Create VM
-$cred = Get-Credential
-New-AzVM -Name $vmName -ResourceGroupName $resourceGroup -Location $location -Credential $cred
+# Create VM config
+Write-Host "Creating virtual machine config..."
+$cred = New-Object System.Management.Automation.PSCredential ($adminUsername, $adminPassword)
 
+New-AzVM `
+  -ResourceGroupName $resourceGroup `
+  -Name $vmName `
+  -Location $location `
+  -VirtualNetworkName "$vmName-VNet" `
+  -SubnetName "$vmName-Subnet" `
+  -SecurityGroupName "$vmName-NSG" `
+  -PublicIpAddressName "$vmName-PublicIP" `
+  -Credential $cred `
+  -Image "Win2019Datacenter" `
+  -Size $vmSize
+
+Write-Host "Virtual Machine $vmName created successfully in $resourceGroup."
